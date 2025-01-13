1f8db8c06aaf2e9b2c6b60978c600274855382fc
Suraj Bodke <surajbodke25@gmail.com>
2025-01-14 00:39:08 +0530

    Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between ">
-      <div className="flex items-center gap-4 text-2xl font-bold  ">
-        <img src="/logo.png" className="w-8 h-8" alt="" />
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold  ">
+        <Image src="logo.png" alt="r3df1y logo" w={32} h={32} />
         <span>R3DF1Y</span>
-      </div>
+      </Link>
       <div className="md:hidden">
         <div
           className="cursor-pointer text-2xl"
           onClick={() => setOpen((prev) => !prev)}
         >
-          {open ? "X" : "="}
+          {open ? "X" : "≡"}
         </div>
         <div
           className={`w-full h-screen flex flex-col items-center justify-center gap-8
@@ -21,27 +24,27 @@ const Navbar = () => {
                open ? "-right-0" : "-right-[100%]"
              }`}
         >
-          <a href="">Home</a>
-          <a href="">Trending</a>
-          <a href="">Most Popular</a>
-          <a href="">About</a>
-          <a href="">
+          <Link to="/">Trending</Link>
+          <Link to="/">Home</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login
             </button>
-          </a>
+          </>
         </div>
       </div>
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="">Home</a>
-        <a href="">Trending</a>
-        <a href="">Most Popular</a>
-        <a href="">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login
           </button>
-        </a>
+        </>
       </div>
     </div>
   );
