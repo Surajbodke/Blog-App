d4d32f23ee57090d136469cd7a24607bddc1dacc
Suraj Bodke <surajbodke25@gmail.com>
2025-01-20 12:28:48 +0530

    Tenth Commit

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,24 @@ import connectDB from "./lib/connectDB.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
 
 const app = express();
+app.use("/webhooks", webhookRouter);
+app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Something went wrong",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.listen(3000, () => {
   connectDB();
