208c02e6623008447b486ded75280f7d90dadd69
Suraj Bodke <surajbodke25@gmail.com>
2025-01-23 14:54:27 +0530

    Eleventh Commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import webhookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+app.use(clerkMiddleware);
 app.use("/webhooks", webhookRouter);
 app.use(express.json());
 
